f45ecec9d54b4e07fcfc44f3864b7a0753611fd2
Ayushi Kumari <54773236+itsmeayushi97@users.noreply.github.com>
2022-01-13 13:32:14 +0530

    Add files via upload

---
+++ class/DBConnection10.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package DBConnection
+ *
+ * @author TechArise Team
+ *
+ * @email  info@techarise.com
+ *   
+ */
+
+// Database Connection
+class DBConnection {
+    private $_dbHostname = "localhost";
+    private $_dbName = "demo_DB";
+    private $_dbUsername = "root";
+    private $_dbPassword = "";
+    private $_con;
+
+    public function __construct() {
+    	try {
+        	$this->_con = new PDO("mysql:host=$this->_dbHostname;dbname=$this->_dbName", $this->_dbUsername, $this->_dbPassword);    
+        	$this->_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    } catch(PDOException $e) {
+			echo "Connection failed: " . $e->getMessage();
+		}
+
+    }
+    // return Connection
+    public function returnConnection() {
+        return $this->_con;
+    }
+}
+?>
---
+++ class/Event.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * @package Event class
+ *
+ * @author TechArise Team
+ *
+ * @email  info@techarise.com
+ *   
+ */
+
+// include connection class
+include("DBConnection.php");
+// Event
+class Event 
+{
+    protected $db;
+    private $_eventID;
+    private $_title;
+    private $_location;
+    private $_content;
+
+    public function setEventID($eventID) {
+        $this->_eventID = $eventID;
+    }
+    public function setTitle($title) {
+        $this->_title = $title;
+    }
+    public function setLocation($location) {
+        $this->_location = $location;
+    }
+    public function setContent($content) {
+        $this->_content = $content;
+    }
+    
+    // __construct
+    public function __construct() {
+        $this->db = new DBConnection();
+        $this->db = $this->db->returnConnection();
+    }
+
+    // create record in database
+    public function create() {
+		try {
+    		$sql = 'INSERT INTO events (title, location, content)  VALUES (:title, :location, :content)';
+    		$data = [
+			    'title' => $this->_title,
+                'location' => $this->_location,
+			    'content' => $this->_content,
+			];
+	    	$stmt = $this->db->prepare($sql);
+	    	$stmt->execute($data);
+			$status = $this->db->lastInsertId();
+            return $status;
+
+		} catch (Exception $err) {
+    		die("Oh noes! There's an error in the query! ".$err);
+		}
+
+    }
+
+    // update record in database
+    public function update() {
+        try {
+		    $sql = "UPDATE events SET title=:title, location=:location, content=:content WHERE id=:event_id";
+		    $data = [
+			    'title' => $this->_title,
+                'location' => $this->_location,
+                'content' => $this->_content,
+                'event_id' => $this->_eventID,
+			];
+			$stmt = $this->db->prepare($sql);
+			$stmt->execute($data);
+			$status = $stmt->rowCount();
+            return $status;
+		} catch (Exception $err) {
+			die("Oh noes! There's an error in the query! " . $err);
+		}
+    }
+   
+    // get records from database
+    public function getList() {
+    	try {
+    		$sql = "SELECT id, title, location, content, created_date FROM events";
+		    $stmt = $this->db->prepare($sql);
+		    $stmt->execute();
+		    $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            return $result;
+		} catch (Exception $err) {
+		    die("Oh noes! There's an error in the query! " . $err);
+		}
+    }
+    // 
+    public function getEvent() {
+        try {
+            $sql = "SELECT id, title, location, content, created_date FROM events WHERE id=:event_id";
+            $stmt = $this->db->prepare($sql);
+            $data = [
+                'event_id' => $this->_eventID
+            ];
+            $stmt->execute($data);
+            $result = $stmt->fetch(\PDO::FETCH_ASSOC);
+            return $result;
+        } catch (Exception $e) {
+            die("Oh noes! There's an error in the query!");
+        }
+    }
+
+    // delete record from database
+    public function delete() {
+    	try {
+	    	$sql = "DELETE FROM events WHERE id=:event_id";
+		    $stmt = $this->db->prepare($sql);
+		    $data = [
+		    	'event_id' => $this->_eventID
+			];
+	    	$stmt->execute($data);
+            $status = $stmt->rowCount();
+            return $status;
+	    } catch (Exception $err) {
+		    die("Oh noes! There's an error in the query! " . $err);
+		}
+    }
+
+
+}
+?>
